870f2a2e924c6cd098563e3ad62cde9bbdd566f5
StarryMohannad :D <73769579+StarryMohannad@users.noreply.github.com>
2024-06-11 09:44:14 +0300

    imported things from the previous version

--- modules/workspace.nix
+++ modules/workspace.nix
@@ -78,7 +78,6 @@
     xdotool
 
     xmobar
-    dmenu   
   ];
 
  home.persistence."/persist/home/starry".directories = [
@@ -94,33 +93,58 @@
  ];
  home.persistence."/persist/home/starry".allowOther = true;
 
-# Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    ".config/xmonad/xmonad.hs".source = ../files/configs/xmonad.hs;
-    ".config/xmobar.rc".source = ../files/configs/xmobar.rc;
-  };
+ # Home Manager is pretty good at managing dotfiles. The primary way to manage
+ # plain files is through 'home.file'.
+ home.file = {
+   ".config/xmonad/xmonad.hs".source = ../files/configs/xmonad.hs;
+   ".config/xmobar.rc".source = ../files/configs/xmobar.rc;
+ };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/starry/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+ xdg.userDirs = {
+   enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+   desktop = "${config.home.homeDirectory}/.local/Desktop";
+   publicShare = "${config.home.homeDirectory}/.local/Public";
+   templates = "${config.home.homeDirectory}/.local/Templates";
+
+   documents = "${config.home.homeDirectory}/Sync/Documents";
+   music = "${config.home.homeDirectory}/Sync/Music";
+   pictures = "${config.home.homeDirectory}/Sync/Pictures";
+   videos = "${config.home.homeDirectory}/Sync/Videos";
+
+   download = "${config.home.homeDirectory}/Downloads";
+ };
+
+ # Home Manager can also manage your environment variables through
+ # 'home.sessionVariables'. These will be explicitly sourced when using a
+ # shell provided by Home Manager. If you don't want to manage your shell
+ # through Home Manager then you have to manually source 'hm-session-vars.sh'
+ # located at either
+ #
+ #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+ #
+ # or
+ #
+ #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+ #
+ # or
+ #
+ #  /etc/profiles/per-user/starry/etc/profile.d/hm-session-vars.sh
+ #
+ home.sessionVariables = {
+   XDG_DATA_HOME = "$HOME/.local/share";
+   XDG_CONFIG_HOME = "$HOME/.config";
+   XDG_STATE_HOME = "$HOME/.local/state";
+   XDG_CACHE_HOME = "$HOME/.cache";
+
+   EDITOR = "nvim";
+   BROWSER = "firefox";
+
+   XCOMPOSECACHE = "$HOME/.cache/X11/xcompose";
+   HISTFILE = "$HOME/.local/state/bash/history";
+   _JAVA_OPTIONS = "-Djava.util.prefs.userRoot='~/.cache/java'";
+ };
+
+ # Let Home Manager install and manage itself.
+ programs.home-manager.enable = true;
 }
