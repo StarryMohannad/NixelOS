ff8fe9aef01a2dd6ff56eb87e555da790319ad50
StarryMohannad :D <73769579+StarryMohannad@users.noreply.github.com>
2024-06-14 17:36:12 +0300

    *fixes

--- modules/home/xmonad-extras/default.nix
+++ modules/home/xmonad-extras/default.nix
@@ -4,8 +4,8 @@
  # Home Manager is pretty good at managing dotfiles. The primary way to manage
  # plain files is through 'home.file'.
  home.file = {
-   ".config/xmonad/xmonad.hs".source = ./files/configs/xmonad.hs;
-   ".config/xmobar.rc".source = ./files/configs/xmobar.rc;
+   ".config/xmonad/xmonad.hs".source = ./configs/xmonad.hs;
+   ".config/xmobar.rc".source = ./configs/xmobar.rc;
    
    ".local/share/sounds".source = ./sounds;
    ".local/share/xmonad-data".source = ./data;
