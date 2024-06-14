f1db669c81f88f9af3a46285a6a86b12eb034410
StarryMohannad :D <73769579+StarryMohannad@users.noreply.github.com>
2024-06-14 15:00:42 +0300

    DRY

--- flake.nix
+++ flake.nix
@@ -2,58 +2,36 @@
  description = "NixelOS Flake :DD";
      
  inputs = {
+   disko.inputs.nixpkgs.follows = "nixpkgs";
+   disko.url = "github:nix-community/disko";
+
+   firefox-addons.inputs.nixpkgs.follows = "nixpkgs";
+   firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+
+   home-manager.inputs.nixpkgs.follows = "nixpkgs";
+   home-manager.url = "github:nix-community/home-manager";
+
+   impermanence.url = "github:nix-community/impermanence";
    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-   disko = {
-     url = "github:nix-community/disko";
-     inputs.nixpkgs.follows = "nixpkgs";
-   };
-
-   impermanence = {
-     url = "github:nix-community/impermanence";
-   };
-
-   home-manager = {
-     url = "github:nix-community/home-manager";
-     inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-   firefox-addons = {
-     url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-     inputs.nixpkgs.follows = "nixpkgs";
-   }; 
-
    stylix.url = "github:danth/stylix";
-
  };
 
- outputs = {nixpkgs, ...} @ inputs:
- {
-    nixosConfigurations."cherry" = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        inputs.disko.nixosModules.default
-        (import ./disko.nix { device = "/dev/nvme0n1"; })
-
-        ./flavors/cherry/system.nix
-              
-        inputs.stylix.nixosModules.stylix
-        inputs.home-manager.nixosModules.default
-        inputs.impermanence.nixosModules.impermanence
-      ];
-    };
-     nixosConfigurations."black-cherry" = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        inputs.disko.nixosModules.default
-        (import ./disko.nix { device = "/dev/nvme0n1"; })
-
-        ./flavors/black-cherry/system.nix
-              
-        inputs.stylix.nixosModules.stylix
-        inputs.home-manager.nixosModules.default
-        inputs.impermanence.nixosModules.impermanence
-      ];
-    };
+ outputs = {nixpkgs, ...} @ inputs: let
+   GlobalModules = [
+     inputs.disko.nixosModules.default
+     (import ./disko.nix { device = "/dev/nvme0n1"; })
+     inputs.stylix.nixosModules.stylix
+     inputs.home-manager.nixosModules.default
+     inputs.impermanence.nixosModules.impermanence
+   ];
+   in {
+     nixosConfigurations."cherry" = nixpkgs.lib.nixosSystem {
+       specialArgs = {inherit inputs;};
+       modules = GlobalModules ++ [ ./flavors/cherry/system.nix ];
+   };
+   nixosConfigurations."black-cherry" = nixpkgs.lib.nixosSystem {
+     specialArgs = {inherit inputs;};
+     modules = GlobalModules ++ [ ./flavors/black-cherry/system.nix ];
+   };
  };
 }
