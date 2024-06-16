f2b693b23abe66f2b59a1ee15163f6c8e02cede7
StarryMohannad :D <73769579+StarryMohannad@users.noreply.github.com>
2024-06-17 01:14:00 +0300

    hyprlock is now good

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, builitins, ... }:
+{ pkgs, config, builitins, lib, ... }:
 
 {
  # Hyprland (Window Manager)
@@ -40,6 +40,46 @@
      format-paused = "  Paused";
    };
  }];
+ programs.waybar.style = ''
+  * {
+    border: none;
+    border-radius: 0;
+    font-family: "FiraCode Nerd Font";
+    font-size: 10px;
+    min-height: 0;
+  }
+
+  window#waybar {
+    border-bottom: 3px solid #${config.lib.stylix.colors.base02};
+    color: #${config.lib.stylix.colors.base05};
+  }
+
+  tooltip {
+    border: 1px solid #${config.lib.stylix.colors.base02};
+  }
+  tooltip label {
+    color: #${config.lib.stylix.colors.base05};
+  }
+
+  #workspaces button {
+    padding: 0 5px;
+    background: transparent;
+    color: white;
+    border-bottom: 3px solid transparent;
+  }
+
+  #workspaces button.focused {
+    border-bottom: 3px solid #${config.lib.stylix.colors.base07};
+  }
+
+  #mode, #clock, #battery {
+    padding: 0 10px;
+  }
+
+  #mode {
+    border-bottom: 3px solid #${config.lib.stylix.colors.base07};
+  }
+ '';
 
  # Hyprpaper (for wallpaper)
  services.hyprpaper.enable = true;
@@ -52,15 +92,20 @@
  # Hyprlock (for lockscreen)
  programs.hyprlock.enable = true;
  programs.hyprlock.settings.background.path = "~/.local/share/wallpaper.png";
- programs.hyprlock.settings.input-field = [{
-   size = "350, 40";
-   position = "0, -80";
-   monitor = "";
-   dots_center = true;
-   fade_on_empty = false;
-   outline_thickness = 5;
-   shadow_passes = 2;
- }];
+ programs.hyprlock.settings.background.blur_passes = 2;
+ programs.hyprlock.settings.background.blur_size = 4;
+
+ programs.hyprlock.settings.input-field.font_color = "rgb(${config.lib.stylix.colors.base05})";
+ programs.hyprlock.settings.input-field.inner_color = "rgb(${config.lib.stylix.colors.base00})";
+ programs.hyprlock.settings.input-field.outer_color = "rgb(${config.lib.stylix.colors.base01})";
+
+ programs.hyprlock.settings.input-field.size = "350, 40";
+ programs.hyprlock.settings.input-field.position = "0, -80";
+ programs.hyprlock.settings.input-field.monitor = "";
+ programs.hyprlock.settings.input-field.dots_center = true;
+ programs.hyprlock.settings.input-field.fade_on_empty = false;
+ programs.hyprlock.settings.input-field.outline_thickness = 5;
+ programs.hyprlock.settings.input-field.shadow_passes = 2;
 
  # Sourcing Files
  home.file.".local/share/sounds".source = ../files/sounds;
@@ -70,7 +115,6 @@
  
  # Sourcing External Configs
  wayland.windowManager.hyprland.extraConfig = builtins.readFile(../files/configs/hypr.conf);
- programs.waybar.style = builtins.readFile(../files/configs/waybar.css);
 
  # Session Varibles
  home.sessionVariables.NIXOS_OZONE_WL = "1";
