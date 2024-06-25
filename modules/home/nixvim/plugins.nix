ee736973bac1e5747407d8ad2fefa4bdbe5f0064
StarryMohannad :D <73769579+StarryMohannad@users.noreply.github.com>
2024-06-25 12:16:32 +0300

    Neorg fix ??

--- modules/home/nixvim/plugins.nix
+++ modules/home/nixvim/plugins.nix
@@ -3,10 +3,7 @@
 {
  programs.nixvim.plugins.cmp.enable = true;
  programs.nixvim.plugins.neocord.enable = true;
-
  programs.nixvim.plugins.neorg.enable = true;
- programs.nixvim.plugins.neorg.package = pkgs.vimPlugins.neorg;
-
  programs.nixvim.plugins.neogit.enable = true;
  programs.nixvim.plugins.fzf-lua.enable = true;
  programs.nixvim.plugins.telescope.enable = true;
