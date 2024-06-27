571ee63e8c64de345f3761e2227b6280e5094a20
StarryMohannad :D <73769579+StarryMohannad@users.noreply.github.com>
2024-06-27 17:28:41 +0300

    sync

--- modules/home/nixvim/plugins.nix
+++ modules/home/nixvim/plugins.nix
@@ -1,6 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-{
+
+let
+  fromGitHub = ref: rev: repo: pkgs.vimUtils.buildVimPlugin {
+    pname = "${lib.strings.sanitizeDerivationName repo}";
+    version = ref;
+    src = builtins.fetchGit {
+      url = "https://github.com/${repo}.git";
+      ref = ref;
+      rev = rev;
+   };
+  };
+in {
  programs.nixvim.plugins.cmp.enable = true;
  programs.nixvim.plugins.cmp.autoEnableSources = true;
 
@@ -11,8 +22,15 @@
  # programs.nixvim.plugins.lualine.enable = true;
  
  programs.nixvim.plugins.lazy.enable = true;
- programs.nixvim.plugins.lazy.plugins = with pkgs.vimPlugins; [
-   vim-orgmode
+ programs.nixvim.extraPlugins = with pkgs.vimPlugins; [
+   {
+    plugin = orgmode;
+    config = "lua require('orgmode').setup{}";
+   }
    vim-table-mode
+   {
+     plugin = (fromGitHub "HEAD" "ea3e467bef7a2c17998bc471844bbd7bd3438f84" "ranjithshegde/orgWiki.nvim");
+     config = "lua require('orgWiki').setup {wiki_path = { '~/sync/docs/org' }, diary_path = '~/sync/docs/org/diary'}";
+   }
  ];
 }
