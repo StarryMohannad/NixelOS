0ae3db5c16ccfdbe7ff60b703b414e24e5841d8e
StarryMohannad :D <73769579+StarryMohannad@users.noreply.github.com>
2024-06-29 21:08:26 +0300

    Starship Catppuccin Prompt (unreadable)

--- modules/shell/home-sub/starship.nix
+++ modules/shell/home-sub/starship.nix
@@ -33,13 +33,13 @@
 
  programs.starship.settings.username = {
   show_always = true;
-  style_user = "bg:#${config.lib.stylix.colors.base09} fg:#${config.lib.stylix.colors.base05}";
-  style_root = "bg:#${config.lib.stylix.colors.base08} fg:#${config.lib.stylix.colors.base05}";
+  style_user = "bg:#${config.lib.stylix.colors.base09} fg:#${config.lib.stylix.colors.base00}";
+  style_root = "bg:#${config.lib.stylix.colors.base08} fg:#${config.lib.stylix.colors.base00}";
   format = "[ $user ]($style)";
  };
 
  programs.starship.settings.directory = { 
-   style = "fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0A}";
+   style = "fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0A}";
    format = "[ $path ]($style)";
    truncation_length = 3;
    truncation_symbol = "…/";
@@ -61,66 +61,66 @@
  programs.starship.settings.git_branch = {
    symbol = " ";
    style = "bg:#${config.lib.stylix.colors.base0B}";
-   format = "[[ $symbol $branch ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0B})]($style)";
+   format = "[[ $symbol $branch ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0B})]($style)";
  };
 
  programs.starship.settings.git_status = {
    style = "bg:#${config.lib.stylix.colors.base0B}";
-   format = "[[($all_status$ahead_behind )](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0B})]($style)";
+   format = "[[($all_status$ahead_behind )](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0B})]($style)";
  };
 
  programs.starship.settings.nodejs = {
    symbol = "";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.c = {
    symbol = " ";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.rust = {
    symbol = "";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.golang = {
    symbol = "";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.php = {
    symbol = "";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.java = {
    symbol = " ";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.kotlin = {
   symbol = "";
   style = "bg:#${config.lib.stylix.colors.base0D}";
-  format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+  format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.haskell = {
    symbol = "";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.python = {
    symbol = "";
    style = "bg:#${config.lib.stylix.colors.base0D}";
-   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base05} bg:#${config.lib.stylix.colors.base0D})]($style)";
+   format = "[[ $symbol( $version) ](fg:#${config.lib.stylix.colors.base00} bg:#${config.lib.stylix.colors.base0D})]($style)";
  };
 
  programs.starship.settings.docker_context = {
